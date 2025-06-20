ab81b6e9a0c64515d3c7e801c0a59c914038e731
Tao <ginadrcoder@gmail.com>
2025-06-20 17:08:48 +1000

    add continuous scrolling layout with image grid

---
+++ client/src/components/ContentCardOne.tsx
@@ -0,0 +1,12 @@
+export default function ContentCardOne() {
+  return (
+    <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow-sm text-blue-900 text-lg">
+      <div className="px-4 py-5 sm:px-6 text-center">
+        Brad Tonge
+      </div>
+      <div className="px-4 py-5 sm:p-6">
+       As a personal trainer, Brad brings over 25 years of experience in promoting physical well-being and nutrition, guiding clients toward achieving their fitness goals. He collaborates with local health professionals to provide comprehensive care that helps you reach your full physical potential. Exercise and nutrition are integral to Brad's lifestyle, along with his deep connection to nature through surfing, running and horticulture. Brad believes that combining structured training in outdoor environments creates the optimal foundation for both mental and physical well-being. Studio training is also available.
+      </div>
+    </div>
+  )
+}
