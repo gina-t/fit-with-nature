c42172c1fe3b805d469c3ee81eb474555ce5accd
Tao <ginadrcoder@gmail.com>
2025-06-20 12:52:34 +1000

    GLTF loader and continupous scroll

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -1,23 +1,58 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { TransitionProvider } from '../context/TransitionContext';
 import TransitionComponent from '../components/Transition';
-import ThreeGraphic from '../views/ThreeGraphic';
+import ContinuousScroll from '../views/ContinuousScroll';
+import Images from '../components/Images';
+import ThreeScene from '../components/ThreeScene';
+import TextOverlay from '../components/TextOverlay';
 
-const Router = () => {
+import gsap from 'gsap';
+import { ScrollSmoother } from 'gsap/ScrollSmoother';
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(ScrollSmoother, useGSAP);
+
+export default function Router() {
+  const location = useLocation();
+
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 1,
+      effects: true,
+      ignoreMobileResize: true,
+      normalizeScroll: true,
+      smoothTouch: 0.1,
+    });
+  }, [location]);
+
   return (
-    <TransitionProvider>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <TransitionComponent>
-              <ThreeGraphic />
-            </TransitionComponent>
-          }
-        />
-      </Routes>
-    </TransitionProvider>
-  );
-};
+    <div id="smooth-wrapper">
+      <div id="smooth-content">
+        {/* Fixed 3D Character Background */}
+        <div className="fixed inset-0 z-0 pointer-events-none">
+          <ThreeScene />
+        </div>
 
-export default Router;
+        <TextOverlay />
+
+        <TransitionProvider>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <TransitionComponent>
+                  <div>
+                    <ContinuousScroll />
+                    <Images />
+                  </div>
+                </TransitionComponent>
+              }
+            />
+          </Routes>
+        </TransitionProvider>
+      </div>
+    </div>
+  );
+}
+
+
